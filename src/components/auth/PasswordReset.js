82ee622b50151bc322d430ad439a2d8d27c85872
Gage Clancy <gclancy121@gmail.com>
2022-12-02 15:48:48 -0600

    finished the authentication section of password reset

--- src/components/auth/PasswordReset.js
+++ src/components/auth/PasswordReset.js
@@ -1,66 +1,73 @@
 import React, {useState, useEffect} from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 import URL from "../../utils/url";
-import { getNextKeyDef } from "@testing-library/user-event/dist/keyboard/getNextKeyDef";
 
 function PasswordReset() {
     const initialFormData = {
         username: '',
-        message: '',
+        security_question: '',
+        security_question_answer: '',
+        message: ''
     }
     const [formData, setFormData] = useState(initialFormData);
-    const [usersName, setUsersName] = useState('');
+    const [passed, setPassed] = useState(false);
 
-    function submitName(evt) {
+    function handleSubmit(evt) {
         evt.preventDefault();
-        const inputtedName = formData.username;
-        axios.get(`${URL}/users/${inputtedName}`).then(res => {
-            const username = res.data.username;
-            if (username === '' || username == null) {
-                setFormData({
-                    ...formData,
-                    message: "That username doesn't exist. Go register!"
-                })
-            } else {
-                setFormData({
-                    ...formData,
-                    message: ''
-                })
-                setUsersName(username);
-            }
-        }).catch (err => {
-            setFormData({
-                ...formData,
-                message: err
-            })
+        const data = {
+            username: formData.username,
+            security_question: formData.security_question,
+            security_question_answer: formData.security_question_answer
+        }
+       axios.post(`${URL}/users/reset-checks`, data).then(result => {
+        setPassed(true);
+       }).catch(err => {
+        setFormData({
+            ...formData,
+            message: err.response.data.message
         })
+       })
     }
-    function nameInput(evt) {
-        setFormData({...formData, [evt.target.name]: evt.target.value})
-    }
-    function QuestionInput() {
-        if (usersName === '') {
+
+    function NavToReset() {
+        if (passed === true) {
             return (
-                <></>
+                <p>Identity validated. Click <Link to='/password-reset-2'>here</Link> to reset your password.</p>
             )
         } else {
             return (
-                <div>
-                    TEST
-                </div>
+                <></>
             )
         }
     }
+
+    function handleChange(evt) {
+        setFormData({...formData, [evt.target.name]: evt.target.value})
+    }   
     return (
         <div className='reset-container'>
-            <h4>{formData.message}</h4>
-            <form onSubmit={submitName} onChange={nameInput}>
-                <h2>Please put in your username.</h2>
-                <input type='text' name='username' />
+            <h3>{formData.message}</h3>
+            <form onSubmit={handleSubmit}>
+                <h2>Username:</h2>
+                <input type='text' name='username' value={formData.username} onChange={handleChange}/>
+                <h2>Security Question & Answer: </h2>
+                <select value={formData.security_question} onChange={handleChange} name='security_question'>
+                    <option value=''>---Select a Question---</option>
+                    <option value="What is your mother's maiden name?">What is your mother's maiden name?</option>
+                    <option value="What street did you grow up on?">What street did you grow up on?</option>
+                    <option value="What was the name of your first pet?">What was the name of your first pet?</option>
+                    <option value="What high school did you attend?">What high school did you attend?</option>
+                    <option value="What year was your father (or mother) born?">What year was your father (or mother) born?</option>
+                    <option value="What was your favorite food as a child?">What was your favorite food as a child?</option>
+                    <option value="What was the model of your first car?">What was the model of your first car?</option>
+                    <option value="What city did you meet your spouse/significant other in?">What city did you meet your spouse/significant other in?</option>
+                </select>
+                <input type="text" value={formData.security_question_answer} onChange={handleChange} name='security_question_answer' />
                 <input type='submit' />
+                <NavToReset />
             </form>
-            <QuestionInput />
         </div>
     )
 }
